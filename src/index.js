e3a07d58149f48cf64770c1430356b1d159b7feb
alexander16108 <alexandermayowa05@gmail.com>
2021-08-03 13:02:35 +0100

    Adding basic CSS styling to css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 window.addEventListener('load', () => {
-  const list = document.querySelector('ordered-score');
+  const list = document.querySelector('.ordered-score');
   new Array(6).fill(1).forEach((_, index) => {
     const li = document.createElement('li');
     li.innerText = `Name: ${index}`;
