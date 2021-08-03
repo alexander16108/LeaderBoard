fd26dd3cbd012724c7b09cc75e16c82864e46715
alexander16108 <alexandermayowa05@gmail.com>
2021-08-03 15:27:04 +0100

    Giving basic instruction for code to run in javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,46 @@
 const path = require('path');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const options = {};
 
 module.exports = {
   mode: 'development',
+
   entry: './src/index.js',
   output: {
-    filename: 'main.bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  devServer: {
-    contentBase: './dist',
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'LEADERBOARD-1',
       template: './src/index.html',
     }),
+    new WebpackManifestPlugin(options),
   ],
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: './dist',
   },
 };
