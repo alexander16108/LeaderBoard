c3fb1bdee4ca9986254bf43e9c777f4be340156f
alexander16108 <alexandermayowa05@gmail.com>
2021-08-03 12:34:59 +0100

    Giving basic instruction for code to run in javascript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 window.addEventListener('load', () => {
-  const ol = document.querySelector('odered-score');
+  const ol = document.querySelector('ordered-score');
   new Array(6).fill(1).forEach((_, index) => {
     const li = document.createElement('li');
     li.innerText = `Name: ${index}`;
