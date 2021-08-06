986d7b6a7f6b20f5f6c75675d913bb9264b28143
alexander16108 <alexandermayowa05@gmail.com>
2021-08-06 13:21:46 +0100

    making basic styling in css for UX

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import {
 const displayResult = () => {
   const ul = document.getElementById('scores');
   const li = document.createElement('li');
+  // const div = document.createElement('div');
+  // div.classList.add('contain');
+  // const pop = document.createElement('p')
+  // pop.innerText = 'input  has been added';
+  // pop.id = 'displayElement';
   ul.append(li);
+  // div.append(ul)
 
   getGame();
   refreshPage();
